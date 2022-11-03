8ef48e3e776a323099f42c440ed16105c0d18be9
Sugam Kumar Singh <72367458+SugamKumarSingh@users.noreply.github.com>
2022-11-03 13:02:43 +0530

    added SplashScreen

---
+++ app/src/main/java/com/example/imagepro/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.imagepro;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, CameraActivity.class));
+                finish();
+            }
+        }, 1200);
+    }
+}
